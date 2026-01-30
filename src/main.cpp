87be2f051c9fd4eba6f05f63fd26eb9dbedca159
IlushkaDV <magnum301076@gmail.com>
2026-01-31 02:36:33 +1000

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@ int main() {
     std::string message = "Hello, World!";
     std::cout << message << std::endl;
     std::cout << "Compiled on: " << __DATE__ << " " << __TIME__ << std::endl;
-    std::cout << "Platform: Linux (Ubuntu)" << std::endl;
+    std::cout << "Platforms: Linux (Ubuntu) and Windows" << std::endl;
     return 0;
 }
